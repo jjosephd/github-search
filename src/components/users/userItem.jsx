fccdeda02a23a99185f59f3c552ca92ff6c8377c
JordanD <jjosephd804@gmail.com>
2023-07-20 18:24:13 -0400

    UserItem card component created using MUI library

--- src/components/users/userItem.jsx
+++ src/components/users/userItem.jsx
@@ -12,17 +12,37 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 import PropTypes from 'prop-types'
+import { Card, CardContent } from '@mui/material'
+import Avatar from '@mui/material/Avatar';
+
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import { Link } from 'react-router-dom';
 
 
-const UserItem = ({user}) => {
+const UserItem = ({user: {login, avatar_url}}) => {
   return (
-    <div>
-      {user.login}
-    </div>
+    <Card sx={{
+      maxWidth: 345,
+      backgroundColor: '#2a303c'}}>
+      <CardContent sx={{  display: 'flex', flexDirection: 'row' }}>
+        <Avatar src={avatar_url} alt='Profile' />
+        <Container maxWidth='sm' sx={{display: 'flex', flexDirection: 'column'}}>
+          <Typography variant="" gutterBottom sx={{
+            fontWeight: 600,
+          color: 'white'}}>
+            {login}
+          </Typography>
+          <Link className='text-base-content text-opacity-40' to={`/users/${login}`}>
+            View Profile
+          </Link>
+          </Container>
+        </CardContent>
+    </Card>
   )
 }
 
-UserItem.PropTypes = {
+UserItem.propTypes = {
   user: PropTypes.object.isRequired,
 }
 
